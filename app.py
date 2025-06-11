e6206d2f6c151995c33150bb35eca1ebe8244df1
Md Hasanuzzaman <dr.has82@gmail.com>
2025-06-11 23:56:35 +0600

    Update

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 # Import libraries
-from flask import Flask, jsonify, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
+
 # Instantiate Flask functionality
 app = Flask("TransactionApp")
+
 # Sample data
 transactions = [
     {'id': 1, 'date': '2023-06-01', 'amount': 100},
@@ -13,6 +15,7 @@ transactions = [
 @app.route('/', methods=['GET'])
 def get_transactions():
     return render_template('transactions.html', transactions=transactions)
+
 
 # Create operation
 @app.route('/add_transaction', methods=['GET', 'POST'])
@@ -28,6 +31,7 @@ def add_transaction():
     else:
         return render_template('form.html')
 
+
 # Update operation
 @app.route('/edit/<int:transaction_id>', methods=['GET', 'POST'])
 def edit_transaction(transaction_id):
@@ -41,13 +45,15 @@ def edit_transaction(transaction_id):
         transaction['date'] = request.form.get('date', transaction['date'])
         transaction['amount'] = int(request.form.get('amount', transaction['amount']))
     return redirect(url_for('get_transactions'))
-            
+       
+   
 # Delete operation
 @app.route('/delete/<int:transaction_id>', methods=['POST'])
 def delete_transaction(transaction_id):
     global transactions
     transactions = [trans for trans in transactions if trans['id'] != transaction_id]
     return redirect(url_for('get_transactions'))
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=9001)
